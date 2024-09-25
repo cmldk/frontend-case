2f238487fe59db0daf05362704c01eb022bbb52b
cmldk <cemal.dak@gmail.com>
2024-09-25 21:27:47 +0300

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Header from '@/components/Header/Header';
 import Sneakers from '@/components/Sneakers/Sneakers';
 import { ShoesCollected } from '@/components/ShoesCollected/ShoesCollected';
 import { Best } from '@/components/Best/Best';
-import { WhyJoinUs } from '@/components/WhyJoinUs/WhyJoinUs';
 import { TheyLoveUs } from '@/components/TheyLoveUs/TheyLoveUs';
 
 export default function Home() {
